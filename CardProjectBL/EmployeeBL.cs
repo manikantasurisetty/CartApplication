5eda751877c9e9ff720674f638b792816e33e189
Manikanta Surisetty <mani.ece444@gmail.com>
2020-06-01 06:17:36 +0530

    Backedncode added

---
+++ CardProjectBL/EmployeeBL.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CartProjectDAL;
+using CartProjectCommonLayer;
+using Newtonsoft.Json;
+namespace CardProjectBL
+{
+    public class EmployeeBL
+    {
+        public DataTable GetStudentBelow5()
+        {
+            StudentDAL studentDAL = new StudentDAL();
+
+
+            return studentDAL.FetchSelectedStudents().Tables[0];
+
+
+
+        }
+
+
+    }
+
+}
